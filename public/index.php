ecf0572c057c61e0aa33aa1d27561321786da88b
greetingsgoods <blololob078@gmail.com>
2021-06-20 09:25:49 +0300

    add class router

--- public/index.php
+++ public/index.php
@@ -1,12 +1,7 @@
 <?php
 require_once "../Views/header.php";
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-	echo "You've submitted the form.";
+    echo "You've submitted the form.";
 }
 require_once "../Views/entity_form.php";
 require_once "../Views/footer.php";
-
-
-
-
-
